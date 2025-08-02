b079a06ac2265df4fda23548e71103782d859f8e
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2025-08-02 16:41:14 +0300

    Finish work

--- test/unit/sortByName.test.js
+++ test/unit/sortByName.test.js
@@ -15,3 +15,36 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+
+describe("Books names test suit", () => {
+  it("Books names should not be sorted if contain repetitions", () => {
+    expect(
+      sorting.sortByName([
+        "Властелин Колец",
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
+it("Achieving 100% code coverage by tests", () => {
+  const input = [
+    "Властелин Колец",
+    "Властелин Колец",
+    "Властелин Колец",
+  ];
+  const expected = [
+    "Властелин Колец",
+    "Властелин Колец",
+    "Властелин Колец",
+  ];
+  const output = sorting.sortByName(input);
+  expect(output).toEqual(expected);
+});
